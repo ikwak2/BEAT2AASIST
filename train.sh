19133b7df4d62452933f09f6b049d7e9f621aaed
konnect714 <konnect714@gmail.com>
2026-05-13 23:59:29 +0000

    upload

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@ pip install pandas
 pip install timm==0.9.12
 
 
-python train_BEAET2AASIST.py --root_path '/Data/data/sanghyeok/IJCAI_OFFICIAL' --pre_trained_path '/Data/data/sanghyeok/IJCAI/networks/BEATs_iter3.pt' --track 1 --feature_split 'Frequency' --multi_layer_fusion 'CNN-gate' --top_k 4
+python train_BEAET2AASIST.py --root_path '/Data/data/sanghyeok/IJCAI_OFFICIAL' --pre_trained_path '/Data/data/sanghyeok/IJCAI_OFFICIAL/networks/BEATs_iter3.pt' --track 1 --feature_split 'Frequency' --multi_layer_fusion 'CNN-gate' --top_k 4
